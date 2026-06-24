b78a470854635b5a81b7216338462a0be83e5318
Meybi Kerimli <kerimovmeybi@gmail.com>
2026-06-24 10:46:06 +0300

    Setup React Router

---
+++ ecommerce-store/src/routes/AppRoutes.tsx
@@ -0,0 +1,19 @@
+import {Routes, Route} from "react-router-dom";
+
+import Home from "../pages/Home";
+import ProductDetails from "../pages/ProductDetails";
+import Cart from "../pages/Cart";
+import Favorites from "../pages/Favorites";
+
+function AppRoutes () {
+    return(
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="favorites" element={<Favorites />} />
+        </Routes>
+    );
+}
+
+export default AppRoutes;
