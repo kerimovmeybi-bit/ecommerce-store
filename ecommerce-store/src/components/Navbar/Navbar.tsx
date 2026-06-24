b797a819807ffd1fb94912baf60fdd4560915ec5
Meybi Kerimli <kerimovmeybi@gmail.com>
2026-06-24 12:26:56 +0300

    Create layout with navbar and footer

---
+++ ecommerce-store/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,35 @@
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography
+          variant="h6"
+          sx={{
+            flexGrow: 1,
+          }}
+        >
+          E-Commerce Store
+        </Typography>
+
+        <Box>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+
+          <Button color="inherit" component={Link} to="/favorites">
+            Favorites
+          </Button>
+
+          <Button color="inherit" component={Link} to="/cart">
+            Cart
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Navbar;
--- ecommerce-store/src/routes/AppRoutes.tsx
+++ ecommerce-store/src/routes/AppRoutes.tsx
@@ -1,19 +1,23 @@
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
+import Layout from "../components/Layout/Layout";
 
 import Home from "../pages/Home";
 import ProductDetails from "../pages/ProductDetails";
 import Cart from "../pages/Cart";
 import Favorites from "../pages/Favorites";
 
-function AppRoutes () {
-    return(
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/product/:id" element={<ProductDetails />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="favorites" element={<Favorites />} />
-        </Routes>
-    );
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default AppRoutes;
